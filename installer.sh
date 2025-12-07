621a716e2777b955f690c74d761375cb49224ecc
tamj <104643052+nagbabalik@users.noreply.github.com>
2025-12-07 09:33:43 +0800

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,12 +1,23 @@
-read -p "Enter installation password: " PASS
+#!/bin/bash
+
+echo -n "Enter installation password: "
+read PASS
 
 curl -sL -H "X-Access-Key: $PASS" \
-  https://jerico-secured-c3f9.mjtsystem.workers.dev -o install
+  https://jerico-secured-c3f9.mjtsystem.workers.dev -o realinstall.sh
 
-if grep -q "Unauthorized" install.sh; then
+# Wrong pass
+if grep -q "Unauthorized" realinstall.sh; then
     echo "❌ Wrong password!"
-    rm -f install.sh
+    rm -f realinstall.sh
     exit 1
 fi
 
-bash install.sh
+# Worker returned 404 HTML?
+if grep -q "<html>" realinstall.sh; then
+    echo "❌ Worker returned error (404/403)"
+    cat realinstall.sh
+    exit 1
+fi
+
+bash realinstall.sh
